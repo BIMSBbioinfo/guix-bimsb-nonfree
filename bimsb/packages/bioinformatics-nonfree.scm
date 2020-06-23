881c7e9d54a43d1e5c7912a17aa90ec71ecff462
Ricardo Wurmus <rekado@elephly.net>
2020-06-23 15:04:02 +0200

    nonfree: gsl-1.16: Move to (bimsb packages variants).
    * bimsb/packages/bioinformatics-nonfree.scm (gsl-1.16): Remove variable.

--- bimsb/packages/bioinformatics-nonfree.scm
+++ bimsb/packages/bioinformatics-nonfree.scm
@@ -1350,29 +1350,6 @@ and clustering) is an R package to infer Gene Regulatory Networks and
 cell types from single-cell RNA-seq data.")
       (license (nonfree:non-free "Non-commercial")))))
 
-(define-public gsl-1.16
-  (package
-    (name "gsl")
-    (version "1.16")
-    (source (origin
-              (method url-fetch)
-              (uri (string-append "mirror://gnu/gsl/gsl-"
-                                  version ".tar.gz"))
-              (sha256
-               (base32
-                "0lrgipi0z6559jqh82yx8n4xgnxkhzj46v96dl77hahdp58jzg3k"))))
-    (build-system gnu-build-system)
-    (arguments `(#:tests? #f))
-    (home-page "https://www.gnu.org/software/gsl/")
-    (synopsis "Numerical library for C and C++")
-    (description
-     "The GNU Scientific Library is a library for numerical analysis in C
-and C++.  It includes a wide range of mathematical routines, with over 1000
-functions in total.  Subject areas covered by the library include:
-differential equations, linear algebra, Fast Fourier Transforms and random
-numbers.")
-    (license license:gpl3+)))
-
 (define-public cermit
   (package
     (name "cermit")
